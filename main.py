d3a6eef8019bb792b76c3f3c7f249af94b099074
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2025-03-14 13:45:11 +0700

    final

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sclassdev!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/sclassdev/app_blog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def view(request):
+    return render(request, 'app_blog/index.html')
